c52b5d5c301e284c444da83ffc3cb2aaf550c5b6
gabrieljakubek <gabrieljakubek@gmail.com>
2020-09-10 01:33:41 -0300

    Septimo commit

--- src/app/servicios/mi-http/mi-http.service.ts
+++ src/app/servicios/mi-http/mi-http.service.ts
@@ -1,12 +1,12 @@
-import { log } from "util";
-import { Injectable } from "@angular/core";
+import { log } from 'util';
+import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpResponse } from "@angular/common/http";
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
-import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/catch";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/toPromise";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class MiHttpService {
